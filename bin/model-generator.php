04108a7426353cfd87227c43adb6d8b17ac4f84a
lytc <prtran@gmail.com>
2013-08-16 13:42:37 +0700

    update

--- bin/model-generator.php
+++ bin/model-generator.php
@@ -3,7 +3,7 @@
 use Lazy\Db\Connection;
 use Lazy\Db\Generator;
 
-define('ROOT', realpath(__DIR__ . '/..'));
+define('ROOT', realpath(__DIR__ . '/../'));
 
 require_once ROOT . '/vendor/autoload.php';
 require_once ROOT . '/application/configs/database.php';
